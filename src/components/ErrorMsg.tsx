5ea6ddb2c39a8ffeb96152d8e7d5b83f79b423d2
Somaye Shafiee <s.shafiee76@gmail.com>
2023-11-12 20:11:32 +0330

    fix bugs

--- src/components/ErrorMsg.tsx
+++ src/components/ErrorMsg.tsx
@@ -1,12 +1,14 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import ErrorIcon from '@mui/icons-material/Error';
-
-const ErrorMsg = ({ message }) => {
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import ErrorIcon from "@mui/icons-material/Error";
+type Props = {
+  message: string | undefined;
+};
+const ErrorMsg = ({ message }: Props) => {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: '5px', mt: '6px' }}>
-      <ErrorIcon style={{ Color: 'red' }} sx={{ width: '20px' }} />
-      <Typography color="red" variant="span" fontSize="14px" fontStyle="italic">
+    <Box sx={{ display: "flex", alignItems: "center", gap: "5px", mt: "6px" }}>
+      <ErrorIcon style={{ color: "red" }} sx={{ width: "20px" }} />
+      <Typography color="red" fontSize="14px" fontStyle="italic">
         {message}
       </Typography>
     </Box>
